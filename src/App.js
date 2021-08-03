bbe8dbb6d20e7adb984d683c8a20a678817c6ad8
Kishan Kumar Singh <Kishan@jivrus.com>
2021-08-03 13:29:59 +0530

    Firts commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
 import CKEditor from "@ckeditor/ckeditor5-react"
-import parse from "html-react-parser"
-import React, { useState } from "react"
+//import parse from "html-react-parser"
+import React, { useState,useEffect } from "react"
 import "./App.css"
-
 function App() {
+
   const [text, setText] = useState("")
+  const [allMemes, setMemes] = useState({allMemes:[]})
+  const [memeSearchText,setMemeSearchText] = useState("");
+  const [parseText,setParseText] = useState("");
+  const editorConfiguration = {
+    toolbar: [ 'bold', 'italic', 'underline', 'link', 'insertImage' ]
+  };
+  
+//if there are any memeSearchText in the editor make an api call to the memes api
+//and filter out the meme for given  memeSearchText.
+  useEffect(()=>{
+    if(memeSearchText.length){
+      fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(data => {
+        setMemes({
+          allMemes: data.data.memes.filter(meme => meme.name.toLowerCase().includes(memeSearchText))
+        })
+
+        let replacedText;
+        //replace the memeSearchPattern with the meme.
+        if(allMemes.allMemes.length)
+        replacedText = parseText.replace(`{{${memeSearchText}_meme}}`,`<img  alt="meme" width="30" height="30" src=${allMemes.allMemes[0].url}></img>`)
+        
+        setMemeSearchText("");
+        if(replacedText){
+          //setParseText(replacedText);
+          setText(replacedText);
+        }
+    })
+    }
+  })
+
+  const editorChanged=(event, editor) => {
+    const data = editor.getData()
+    setParseText(data)
+    //setText(data)
+  }
+
+//Function to find the search pattern for text and extract the text for filtering memes
+  const generateMemes =()=>{
+    let memeSearchPattern = parseText.match(/\{\{\w+_meme\}\}/gmi)
+    //console.log(memeSearchPattern);
+    if(memeSearchPattern){
+      memeSearchPattern.forEach(ele =>{
+      ele = ele.replace(/\{\{|_meme|\}\}/gmi,"");
+      //console.log(ele);
+      setMemeSearchText(ele);
+      })
+    }
+  }
+
   return (
     <div className="App">
       <div className="editor">
         <CKEditor
-          editor={ClassicEditor}
+          editor={Editor}
+          config={ editorConfiguration }
           data={text}
-          onChange={(event, editor) => {
-            const data = editor.getData()
-            setText(data)
-          }}
+          onChange={(e,editor)=>editorChanged(e,editor)}
         />
       </div>
-      <div>
-        <h2>Content</h2>
-        <p>{parse(text)}</p>
+      <br></br>
+      <div className="meme">
+        <button onClick={generateMemes}>GET Memes</button>
       </div>
     </div>
   )
 }
-
 export default App
